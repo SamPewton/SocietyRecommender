b0934cec5536403183ccaf27108472adef99c027
Gudaitis <eg_0108@hotmail.co.uk>
2021-03-29 15:24:41 +0100

    login page

--- USF_v2/app/src/main/java/com/example/usf_v2/MainActivity.java
+++ USF_v2/app/src/main/java/com/example/usf_v2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.usf_v2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText userName = findViewById(R.id.editTextTextEmailAddress);
+        EditText password = findViewById(R.id.editTextTextPassword);
+        Button loginButton = findViewById(R.id.button);
+
+
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }

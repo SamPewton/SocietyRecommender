2da265fe3fd94669b1701a20d62db758ad8d78b1
Gudaitis <eg_0108@hotmail.co.uk>
2021-03-31 14:45:16 +0100

    frontend work - edgar
    added new page and updated title page.

--- USF_v2/app/src/main/java/com/example/usf_v2/MainActivity.java
+++ USF_v2/app/src/main/java/com/example/usf_v2/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.usf_v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.*;
 
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String[] userInputtedUN = {userName.getText().toString()};
+                String displayName = userName.getText().toString();
                 //if the user gets both username and password correct
                 if (sql.checkLogin(userInputtedUN, password.getText().toString())) {
                     //sets the global loggedInUser variable to the name of the user
@@ -40,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
                     //!!if logs in ok, move onto the next page!!//
                     System.out.println("Logged in ok");
+                    String welcome = "Welcome " + displayName;
+                    Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+                    loginComplete();
                 }
                 //else if the username is not in the database, create account
                 //write other else?
@@ -47,11 +53,17 @@ public class MainActivity extends AppCompatActivity {
                 else {
                     //show the login failed and reprompt
                     System.out.println("Failed login attempt");
+                    String wrongPass = "You have entered the wrong user or password";
+                    Toast.makeText(getApplicationContext(), wrongPass, Toast.LENGTH_LONG).show();
                 }
 
             }
         });
 
-
+    }
+    public void loginComplete()
+    {
+        Intent intent = new Intent(this, mainMenu.class);
+        startActivity(intent);
     }
 }

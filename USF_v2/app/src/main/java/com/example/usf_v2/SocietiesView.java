5fcec3613c881bf5e6b714e7d992f1f8b69fc7aa
Gudaitis <eg_0108@hotmail.co.uk>
2021-04-21 15:43:38 +0100

    finished display page
    added society textboxes which now display random societies

--- USF_v2/app/src/main/java/com/example/usf_v2/SocietiesView.java
+++ USF_v2/app/src/main/java/com/example/usf_v2/SocietiesView.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class SocietiesView extends AppCompatActivity {
     String un = "";
@@ -19,6 +22,11 @@ public class SocietiesView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_societies_view);
         Button MenuButton = findViewById(R.id.ButtonMenuBack);
+        TextView soc1 = findViewById(R.id.textView11);
+        TextView soc2 = findViewById(R.id.textView12);
+        TextView soc3 = findViewById(R.id.textView13);
+        TextView soc4 = findViewById(R.id.textView14);
+        TextView soc5 = findViewById(R.id.textView15);
         Intent passedIn = getIntent();
         un = passedIn.getStringExtra("user_name");
         pw = passedIn.getStringExtra("user_pw");
@@ -39,10 +47,26 @@ public class SocietiesView extends AppCompatActivity {
         }
 
 
+        List<String> keys = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+        List<TextView> socDisplay = new ArrayList<>();
+        socDisplay.add(soc1);
+        socDisplay.add(soc2);
+        socDisplay.add(soc3);
+        socDisplay.add(soc4);
+        socDisplay.add(soc5);
+
         //example of how to access hashmap for the data
         //this needs changing from a console print to viewing on the display
         for(String s : allSocieties.keySet()) {
             System.out.println(s + " " + allSocieties.get(s));
+            keys.add(s);
+            values.add(allSocieties.get(s));
+        }
+
+        for(int i = 0; i<socDisplay.size(); i++)
+        {
+            socDisplay.get(i).setText(keys.get(i) + "\n" + "\n" + values.get(i));
         }
 
 
@@ -61,4 +85,7 @@ public class SocietiesView extends AppCompatActivity {
         intent.putExtra("user_pw", pw);
         startActivity(intent);
     }
+
+
+
 }
